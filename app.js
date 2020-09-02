24db2ccfe7e2fadf6a2cb083d01bd6c175accbd4
gifflearn <jflahellec.learn@gmail.com>
2020-09-02 13:36:32 +0200

    Add files via upload
    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// ekko lightbox
+$(Document).on('click','[data-toggle="lightbox"]',function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+// Carousel de texte
+$('.carousel').carousel({
+    interval: 2500,
+    pause: 'null'
+})
+
+// Navbar animation
+$(window).scroll(function(){
+    if($(this).scrollTop() > 30) {
+        $('.navbar').addClass('opaque');
+    } else {
+        $('.navbar').removeClass('opaque');
+    }
+})
